65a30f5444242b48d9c999936fb1d6ebf54bec5d
rekki.katou <hassoubeat.work@gmail.com>
2024-02-29 17:55:32 +0900

    Add css modules layout

--- app/routes/ant_design.tsx
+++ app/routes/ant_design.tsx
@@ -1,11 +1,13 @@
 import { Link } from "@remix-run/react";
 import { Button } from 'antd';
+import styles from '~/styles/ant_design.module.css';
 
 export default function Index() {
   return (
     <>
       <h1>Test AntDesign</h1>
       <Button type="primary">test</Button>
+      <Button type="primary" className={styles.blue}>test</Button>
       <div>
         <Link to="/">to top</Link>
       </div>
